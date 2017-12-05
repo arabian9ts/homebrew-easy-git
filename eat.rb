dae4f9304d1c730bc6f6e28b5756c71309b9191b
arabian9ts <nok14.tm59@gmail.com>
2017-12-05 22:13:46 +0900

    add: install script

--- eat.rb
+++ eat.rb
@@ -3,7 +3,7 @@ require "formula"
 class Eat < Formula
   homepage "https://github.com/arabian9ts/easy-git"
   url "https://github.com/arabian9ts/easy-git/releases/download/v0/eat.tar.gz"
-  sha256 "6088aa271d1ee929e4accb1a295bee37e7c16f159f49c0cc9dc84efddc182034"
+  sha256 "b7fcc888a45aa536e0d8f6caef949560aa8ede391a4f4a8015d2bee37ca51bde"
   head "https://github.com/arabian9ts/easy-git.git"
   version "v0"
 
